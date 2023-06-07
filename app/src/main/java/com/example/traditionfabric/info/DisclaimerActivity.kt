b9b1a4dcf5015841c68c80117617712bfaaf21cd
unknown <guloeniria3@gmail.com>
2023-06-08 06:28:13 +0700

    aplikasi trafa

---
+++ app/src/main/java/com/example/traditionfabric/info/DisclaimerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trafa.ui.info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trafa.R
+
+class DisclaimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disclaimer)
+    }
+}
